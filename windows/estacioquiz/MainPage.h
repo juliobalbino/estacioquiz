dadeaae7439798a8248eb0bfbf7c9f8bddfd0e97
Julio Balbino <julio4110@hotmail.com>
2022-10-30 18:02:54 -0300

    first commit

---
+++ windows/estacioquiz/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::estacioquiz::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::estacioquiz::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
